74d9375a51791170001c841dff6708f0e7331761
Art3YouLi <excel1entxx@126.com>
2023-03-01 22:05:02 +0800

    完善第二天学习：补充学习类型转换和二进制原码反码补码知识

--- codes/src/com/art3youli/convertType/typeDemo2.java
+++ codes/src/com/art3youli/convertType/typeDemo2.java
@@ -13,5 +13,17 @@ public class typeDemo2 {
         byte f = 10;
         f = (byte)e;
         System.out.println(f);
+
+        byte g = 10;
+        byte h = 20;
+        // 如下会报错：
+        // byte i = g + h;
+        int i = g + h;
+        System.out.println(i);
+
+        int j = 1;
+        char k = 'a';
+        int l = j + k;
+        System.out.println(l);
     }
 }
