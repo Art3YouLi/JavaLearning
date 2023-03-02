b6b2ccf282cb078624c99ac18150f3a4a6d4731a
Art3YouLi <excel1entxx@126.com>
2023-03-02 22:40:18 +0800

    开始第三天学习：补充部分注释

--- codes/src/com/art3youli/hello/HelloWorld.java
+++ codes/src/com/art3youli/hello/HelloWorld.java
@@ -1,4 +1,4 @@
-package com.art3youli.hello;
+package com.art3youli.hello; // 代码层面说明类所属于的包
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
